69186338e181c4c79c5c4d644b445d12cac4059b
Ha Quanq <quangha0120010@gmail.com>
2025-10-29 20:59:34 +0700

    Add favicon, change page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor Solutions Portal",
+  icons: { icon: "/favicon-32x32.png" },
+};
 
 export default function RootLayout({
   children,
