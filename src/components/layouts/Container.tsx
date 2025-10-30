daa19e79e0c8f0547aa07d82564ffd9d3aea735c
Ha Quanq <quangha0120010@gmail.com>
2025-10-30 16:07:14 +0700

    Add Container layout

---
+++ src/components/layouts/Container.tsx
@@ -0,0 +1,5 @@
+import { cn } from "@/utils/cn";
+
+export const Container = ({ className, children, ...restProps }: React.ComponentProps<"div">) => {
+  return <div className={cn("mx-auto w-[min(100vw-3rem,75rem)]", className)} {...restProps}></div>;
+};
