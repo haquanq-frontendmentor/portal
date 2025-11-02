b8d288266f08f806ab57ad4323760d393c1a26b9
Ha Quanq <quangha0120010@gmail.com>
2025-11-03 03:30:14 +0700

    Add arrow icon component

---
+++ src/icons/MoveRightLongIcon.tsx
@@ -0,0 +1,10 @@
+export const MoveRightLongIcon = (props: React.ComponentProps<"svg">) => {
+  return (
+    <svg width="48" height="16" viewBox="0 0 48 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M44.1716 6.77822L38.8076 1.41421L40.2218 0L48 7.77822L40.2218 15.5563L38.8076 14.1421L44.1716 8.77822L0 8.77817V6.77822H44.1716Z"
+        fill="#222222"
+      />
+    </svg>
+  );
+};
