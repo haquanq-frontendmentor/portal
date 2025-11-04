8ba29e3b2539cc976060486a247360db8a8e5402
Ha Quanq <quangha0120010@gmail.com>
2025-11-04 15:05:55 +0700

    Fix grammar issue

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -6,7 +6,7 @@ export default function SnippetsPage() {
       <div className="bg-gray-50 py-[clamp(2rem,1.0458rem+4.0712vw,3rem)]">
         <ContainerLayout>
           <h1 className="text-[clamp(2rem,0.0916rem+8.1425vw,4rem)] leading-[clamp(2.5rem,0.5916rem+8.1425vw,4.5rem)] font-bold">
-            Work on progress!
+            Work in progress!
           </h1>
         </ContainerLayout>
       </div>
