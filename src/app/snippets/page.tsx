3e85318af982ff643dd678e949fd04d8ffdc40b1
Ha Quanq <quangha0120010@gmail.com>
2025-11-04 15:46:37 +0700

    Add dark theme styles to /snippets page

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -2,8 +2,8 @@ import { ContainerLayout } from "@/components/layouts/ContainerLayout";
 
 export default function SnippetsPage() {
   return (
-    <main>
-      <div className="bg-gray-50 py-[clamp(2rem,1.0458rem+4.0712vw,3rem)]">
+    <main className="min-h-screen bg-white dark:bg-gray-900">
+      <div className="bg-gray-50/50 py-[clamp(2rem,1.0458rem+4.0712vw,3rem)] dark:bg-gray-800/50 dark:text-gray-50">
         <ContainerLayout>
           <h1 className="text-[clamp(2rem,0.0916rem+8.1425vw,4rem)] leading-[clamp(2.5rem,0.5916rem+8.1425vw,4.5rem)] font-bold">
             Work in progress!
