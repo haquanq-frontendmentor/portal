75e88e5fed2c01f53250eb1e95c18f171e3dae87
Ha Quanq <quangha0120010@gmail.com>
2025-11-04 15:57:36 +0700

    Add dark theme to TextTooltip

--- src/components/Tooltip/TextTooltip.tsx
+++ src/components/Tooltip/TextTooltip.tsx
@@ -11,7 +11,7 @@ export const TextTooltip = ({ trigger, content }: TextTooltipProps) => {
       <Tooltip.Trigger render={trigger} />
       <Tooltip.Portal>
         <Tooltip.Positioner className="z-50" sideOffset={8}>
-          <Tooltip.Popup className="flex origin-(--transform-origin) flex-col rounded-md bg-[canvas] px-2 py-1 text-sm shadow-lg outline outline-gray-200 transition-[transform,scale,opacity] data-ending-style:scale-90 data-ending-style:opacity-0 data-instant:duration-0 data-starting-style:scale-90 data-starting-style:opacity-0">
+          <Tooltip.Popup className="flex origin-(--transform-origin) flex-col rounded-md bg-white px-2 py-1 text-sm shadow-lg outline outline-gray-200 transition-[transform,scale,opacity] data-ending-style:scale-90 data-ending-style:opacity-0 data-instant:duration-0 data-starting-style:scale-90 data-starting-style:opacity-0 dark:bg-gray-900 dark:text-gray-50 dark:outline-gray-500">
             {content}
           </Tooltip.Popup>
         </Tooltip.Positioner>
