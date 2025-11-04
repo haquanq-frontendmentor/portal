a58a889eece75de492a2c78c473c16a2b4631178
Ha Quanq <quangha0120010@gmail.com>
2025-11-04 14:33:31 +0700

    Use normal images tag instead of next/Image

--- src/app/Appreciation.tsx
+++ src/app/Appreciation.tsx
@@ -1,13 +1,12 @@
 import { ContainerLayout } from "@/components/layouts/ContainerLayout";
 import { appConfig } from "@/configs/appConfig";
-import Image from "next/image";
 
 export const Appreciation = () => {
   return (
     <section className="bg-gray-50/50">
       <ContainerLayout>
         <div className="flex flex-col items-center gap-10 pt-22 pb-30 text-center">
-          <Image src={`${appConfig.basePath}/images/thumb-up-image.png`} alt="" width={250} height={250} />
+          <img src={`${appConfig.basePath}/images/thumb-up-image.png`} alt="" />
           <h2 className="max-w-120 text-[2.5rem] leading-12 font-bold text-gray-900">
             Hey there traveler! Thanks for stopping by.
           </h2>
--- src/app/Intro.tsx
+++ src/app/Intro.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/common/Button";
 import { ContainerLayout } from "@/components/layouts/ContainerLayout";
 import { appConfig } from "@/configs/appConfig";
 import { ArrowUpRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export const Intro = () => {
@@ -11,12 +10,10 @@ export const Intro = () => {
       <ContainerLayout>
         <div className="grid items-center justify-items-center gap-16 pt-12 pb-24 lg:grid-cols-[37.5rem_auto] lg:py-32">
           <div className="flex items-start">
-            <Image
+            <img
               className="lg:col-start-2 lg:max-w-[200%]"
               src={`${appConfig.basePath}/images/intro-image.png`}
               alt=""
-              width={607}
-              height={480}
             />
           </div>
           <div className="flex flex-col items-center text-center lg:row-start-1 lg:items-start lg:text-start">
