b5ca3c438a2e4fddd9ea1ddb7b79aa36b21991b1
Ha Quanq <quangha0120010@gmail.com>
2025-11-04 15:58:24 +0700

    Add dark theme to SolutionCard

--- src/components/Solution/SolutionCard.tsx
+++ src/components/Solution/SolutionCard.tsx
@@ -14,9 +14,9 @@ interface SolutionCardProps {
 export const SolutionCard = ({ solution }: SolutionCardProps) => {
   if (solution.colors.brand === "") solution.colors.brand = "#222";
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col rounded-xl bg-white inset-ring inset-ring-gray-100 dark:bg-gray-800 dark:inset-ring-gray-700">
       <a
-        className="group relative aspect-384/282 rounded-xl"
+        className="group relative aspect-384/282 rounded-xl shadow-lg"
         href={solution.live}
         target="_blank"
         aria-label="Go to live site"
@@ -42,15 +42,17 @@ export const SolutionCard = ({ solution }: SolutionCardProps) => {
           }}
         ></span>
       </a>
-      <div className="flex items-center justify-between gap-3">
-        <p className="font-semi-bold text-base leading-6 tracking-tight text-gray-900">{solution.name}</p>
+      <div className="flex items-center justify-between gap-3 p-4">
+        <p className="font-semi-bold text-base leading-6 tracking-tight text-gray-900 dark:text-gray-50">
+          {solution.name}
+        </p>
         <div className="flex gap-2 *:flex *:aspect-square *:w-8 *:items-center *:justify-center *:rounded-full">
           <Tooltip.Provider>
             <TextTooltip
               content="View code"
               trigger={
                 <a
-                  className="bg-gray-900 px-1.5 text-white"
+                  className="bg-gray-900 px-1.5 text-white inset-ring inset-ring-gray-800 dark:inset-ring-gray-500"
                   href={solution.repository.url}
                   aria-label="View code"
                   target="_blank"
@@ -63,7 +65,7 @@ export const SolutionCard = ({ solution }: SolutionCardProps) => {
               content="View solution post"
               trigger={
                 <a
-                  className="block bg-white px-2 inset-ring inset-ring-gray-200"
+                  className="block bg-white px-2 inset-ring inset-ring-gray-200 dark:inset-ring-gray-500"
                   href={solution.frontendmentor.solutionUrl}
                   aria-label="View solution post"
                   target="_blank"
