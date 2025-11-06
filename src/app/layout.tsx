4cebbbbb3acc2ba5f5e22f69fd80c8005df704f2
Ha Quanq <quangha0120010@gmail.com>
2025-11-07 00:14:48 +0700

    Add meta tags for social media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Frontend Mentor Solutions Portal",
   icons: { icon: appConfig.basePath + "/favicon-32x32.png" },
+  alternates: {
+    canonical: "https://haquanq-frontendmentor.github.io/portal/",
+  },
+  openGraph: {
+    url: "https://haquanq-frontendmentor.github.io/portal/",
+    description:
+      "A collection of my Frontend Mentor challenge solutions that demonstrates responsive UI layouts, attention to details and usages of various technologies. Each project highlights practical problems in modern web development.",
+    locale: "en_US",
+  },
 };
 
 export default function RootLayout({
