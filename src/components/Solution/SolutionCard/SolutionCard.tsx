0986176eea027f6052363ed60559d34dd59b63e4
Ha Quanq <quangha0120010@gmail.com>
2025-11-06 22:34:13 +0700

    Add topics list for SolutionCard with option props

--- src/components/Solution/SolutionCard/SolutionCard.tsx
+++ src/components/Solution/SolutionCard/SolutionCard.tsx
@@ -3,21 +3,31 @@
 import { Solution } from "@/services/Solution/types";
 import { SolutionCardExternalLink } from "./SolutionCardExternalLink";
 import { SolutionCardPreview } from "./SolutionCardPreview";
+import { SolutionCardTopicList } from "./SolutionCardTopicList";
 
 interface SolutionCardProps {
   solution: Solution;
+  showTopics?: boolean;
+  showDifficulty?: boolean;
 }
 
-export const SolutionCard = ({ solution }: SolutionCardProps) => {
+export const SolutionCard = ({ solution, showTopics = false, showDifficulty = false }: SolutionCardProps) => {
   if (solution.colors.brand === "") solution.colors.brand = "#222";
+
   return (
-    <div className="flex flex-col rounded-xl bg-gray-50 inset-ring inset-ring-gray-100 dark:bg-gray-800 dark:inset-ring-gray-700">
+    <div className="flex h-full flex-col rounded-xl bg-gray-50/50 inset-ring inset-ring-gray-100 dark:bg-gray-800/50 dark:inset-ring-gray-700">
       <SolutionCardPreview solution={solution} />
-      <div className="flex items-center justify-between gap-3 p-4">
-        <p className="font-semi-bold text-base leading-6 tracking-tight text-gray-900 dark:text-gray-50">
-          {solution.name}
-        </p>
-        <SolutionCardExternalLink solution={solution} />
+      <div className="flex flex-col divide-y divide-gray-100 dark:divide-gray-700">
+        <div className="flex items-center justify-between gap-5 p-4">
+          <div className="flex flex-col gap-2 text-gray-900 dark:text-gray-50">
+            {showDifficulty && (
+              <p className="pt-2 text-xs tracking-widest uppercase">{solution.frontendmentor.difficulty}</p>
+            )}
+            <p className="font-semi-bold text-base leading-6 tracking-tight">{solution.name}</p>
+          </div>
+          <SolutionCardExternalLink solution={solution} />
+        </div>
+        {showTopics && <SolutionCardTopicList solution={solution} />}
       </div>
     </div>
   );
---
+++ src/components/Solution/SolutionCard/SolutionCardTopicList.tsx
@@ -0,0 +1,37 @@
+import { TextTooltip } from "@/components/Tooltip/TextTooltip";
+import { Button } from "@/components/common/Button";
+import { Solution } from "@/services/Solution/types";
+import { Tooltip } from "@base-ui-components/react";
+
+interface SolutionCardTopicListProps {
+  solution: Solution;
+}
+
+export const SolutionCardTopicList = ({ solution }: SolutionCardTopicListProps) => {
+  return (
+    <div>
+      <Tooltip.Provider>
+        <ul className="flex flex-wrap gap-3 p-4">
+          {solution.topics.map((topic) => (
+            <li key={solution.name + topic}>
+              <TextTooltip
+                content={`Add ${topic} to filter`}
+                trigger={
+                  <Button
+                    className="bg-gray-100 tracking-tight text-gray-600 dark:bg-gray-700"
+                    type="button"
+                    variant="secondary"
+                    size="small"
+                    aria-label={`Add ${topic} to filter`}
+                  >
+                    {`#${topic}`}
+                  </Button>
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </Tooltip.Provider>
+    </div>
+  );
+};
