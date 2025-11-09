7ac2c01a5e8b79b8e82a99766f9d95e707daee38
Ha Quanq <quangha0120010@gmail.com>
2025-11-09 08:00:52 +0700

    Move opengraph-image to public/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://haquanq-frontendmentor.github.io"),
   title: "Frontend Mentor Solutions Portal",
   icons: { icon: appConfig.basePath + "/favicon-32x32.png" },
   alternates: {
     canonical: "https://haquanq-frontendmentor.github.io/portal/",
   },
   openGraph: {
+    images: [appConfig.basePath + "/opengraph-image.jpg"],
     url: "https://haquanq-frontendmentor.github.io/portal/",
     description:
       "A collection of my Frontend Mentor challenge solutions that demonstrates responsive UI layouts, attention to details and usages of various technologies. Each project highlights practical problems in modern web development.",
