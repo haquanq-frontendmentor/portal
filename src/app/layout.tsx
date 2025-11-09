a8ca36581a0d675a9d7d5d509290d3a8949f9deb
Ha Quanq <quangha0120010@gmail.com>
2025-11-09 07:40:59 +0700

    Remove URI from metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://haquanq-frontendmentor.github.io/portal"),
+  metadataBase: new URL("https://haquanq-frontendmentor.github.io"),
   title: "Frontend Mentor Solutions Portal",
   icons: { icon: appConfig.basePath + "/favicon-32x32.png" },
   alternates: {
